cd6a700dc56f6dfc2903ef688f23da3b7d0e8a4c
XiaoLux <dcarlos.mx@gmail.com>
2023-07-31 18:30:11 -0600

    update starts with a random country, city

--- src/context/location.jsx
+++ src/context/location.jsx
@@ -1,14 +1,44 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
-import { getLocation } from "../utils/getLocation";
+
+import citiesList from "../mocks/cities.json";
+import { useEffect } from "react";
 
 // creamos el contexto
 export const LocationContext = createContext();
 
 // luego lo proveemos
 export function LocationProvider({ children }) {
-  const [country, setCountry] = useState(getLocation().country);
-  const [city, setCity] = useState(getLocation().city);
+  const [country, setCountry] = useState(() => {
+    // Get an array of all countries
+    const countries = Object.keys(citiesList);
+
+    // Select a random country
+    return countries[Math.floor(Math.random() * countries.length)];
+  });
+  const [city, setCity] = useState(() => {
+    // Retrieve the cities array for the random country
+    const cities = citiesList[country];
+
+    // Generate a random index within the range of cities array
+    const randomIndex = Math.floor(Math.random() * cities.length);
+
+    const tmpCity = cities[randomIndex];
+
+    return tmpCity;
+  });
+
+  useEffect(() => {
+    // Retrieve the cities array for the random country
+    const cities = citiesList[country];
+
+    // Generate a random index within the range of cities array
+    const randomIndex = Math.floor(Math.random() * cities.length);
+
+    const tmpCity = cities[randomIndex];
+
+    setCity(tmpCity);
+  }, [country]);
 
   return (
     <LocationContext.Provider
