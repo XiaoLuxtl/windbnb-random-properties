dfde4b9ffbd02df6535d11689eba66b70c8f47a0
XiaoLux <dcarlos.mx@gmail.com>
2023-07-31 18:32:09 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useContext } from "react";
 import { Card } from "./components/Card";
 import { Navbar } from "./components/Navbar";
-import { genLocation } from "./utils/genLocation";
+import { GetSuggestedLocation } from "./components/GetSuggestedLocation";
+import { useContext } from "react";
 import { LocationContext } from "./context/location";
+import { GenLocation } from "./utils/genLocation";
 
 function App() {
   const { country } = useContext(LocationContext);
-
-  const { location } = genLocation(6, country);
+  const { location } = GenLocation(6, country);
 
   return (
     <>
       <Navbar />
+      <GetSuggestedLocation />
       <div className="w-11/12 max-w-11/12 md:max-w-screen-xl m-auto">
         <h2 className="py-6 text-gray-900 dark:text-stone-100 text-2xl font-bold">
           Stays in {country}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-fit md:w-full m-auto">
-          {location.stay.map((data) => (
-            <Card key={data.id} data={data} />
-          ))}
+          {location ? (
+            location.stay.map((data) => <Card key={data.id} data={data} />)
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </>
